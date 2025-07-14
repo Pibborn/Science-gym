30d1af7c523e14103b91cdf38cd15a877868c8eb
Mattia Cerrato <mcerrato@uni-mainz.de>
2025-07-14 15:50:18 +0200

    Update docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_wagtail_theme', 'myst_parser', 'sphinx.ext.githubpages']
+extensions = ['myst_parser', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -27,5 +27,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_wagtail_theme'
+html_theme = 'shibuya'
 html_static_path = ['_static']
